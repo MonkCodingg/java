dd9c62b5e0d750b5cb0583a795c2c85a97e6f61f
401ST-012 <401ST-012@DESKTOP-N5H3JDG>
2018-09-28 20:38:24 +0900

    class name change

--- ch02/src/programing/CaculateSquareOfIntegralNumber_02.java
+++ ch02/src/programing/CaculateSquareOfIntegralNumber_02.java
@@ -1,6 +1,6 @@
 package programing;
 
-public class CaculateSquareOfPositiveNumber_02 {
+public class CaculateSquareOfIntegralNumber_02 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
