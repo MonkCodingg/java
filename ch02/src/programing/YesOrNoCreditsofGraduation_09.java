214952abf625965040489eee9d962f9002ab9aa5
Na <Na@Na-PC>
2018-10-01 02:12:26 +0900

    ch02 programming 09 ing

--- ch02/src/programing/YesOrNoCreditsofGraduation_09.java
+++ ch02/src/programing/YesOrNoCreditsofGraduation_09.java
@@ -1,10 +1,22 @@
 package programing;
 
+import java.util.Scanner;
+
 public class YesOrNoCreditsofGraduation_09 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int majorCredit = 0;
+		int cultureCredit = 0;
+		int generalCredit = 0;
 		
+		Scanner myInput = new Scanner(System.in);
+		System.out.println("전공 이수 학점: ");
+		majorCredit= myInput.nextInt();
+		System.out.println("교양 이수 학점: ");
+		System.out.println("일반 이수 학점: ");
+		System.out.println("졸업 가능");
+		System.out.println("졸업 불가능");
 	}
 
 }
