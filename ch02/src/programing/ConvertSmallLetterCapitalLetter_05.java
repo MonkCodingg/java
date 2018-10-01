60a14d3185ee81bc4aa98415695cdc9f88a18924
401ST-012 <401ST-012@DESKTOP-N5H3JDG>
2018-10-01 15:15:34 +0900

    181001 오전 # WARNING: head commit changed in the meantime
    test3

--- ch02/src/programing/ConvertSmallLetterCapitalLetter_05.java
+++ ch02/src/programing/ConvertSmallLetterCapitalLetter_05.java
@@ -1,10 +1,15 @@
 package programing;
 
+import java.util.Scanner;
+
 public class ConvertSmallLetterCapitalLetter_05 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		Scanner myInput = new Scanner(System.in);
+		System.out.println("대문자로 변환할 소문자 입력 => ");
+		String inputChar = myInput.next();
+//		int InputAsciiCode = (int) inputChar;
 	}
 
 }
--- ch02/src/programing/YesOrNoCreditsofGraduation_09.java
+++ ch02/src/programing/YesOrNoCreditsofGraduation_09.java
@@ -14,9 +14,28 @@ public class YesOrNoCreditsofGraduation_09 {
 		System.out.println("전공 이수 학점: ");
 		majorCredit= myInput.nextInt();
 		System.out.println("교양 이수 학점: ");
+		cultureCredit = myInput.nextInt();
 		System.out.println("일반 이수 학점: ");
-		System.out.println("졸업 가능");
-		System.out.println("졸업 불가능");
+		generalCredit = myInput.nextInt();
+		
+		boolean a = (cultureCredit >=30 && generalCredit >=30) || cultureCredit + generalCredit>=80;
+		
+		boolean b = majorCredit >=70;
+		
+		boolean c = majorCredit + cultureCredit + generalCredit >= 140;
+		
+		
+		
+		
+		
+		if(a && c&& b) {
+			System.out.println("졸업 가능");
+			
+		}else
+			System.out.println("졸업 불가능");
+
+		myInput.close();
+
 	}
 
 }
