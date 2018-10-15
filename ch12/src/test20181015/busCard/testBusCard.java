fc542a35adad9da11a860ba9776db55b2c8b84d2
401ST-012 <401ST-012@DESKTOP-N5H3JDG>
2018-10-15 22:04:42 +0900

    ch12 20181015

---
+++ ch12/src/test20181015/busCard/testBusCard.java
@@ -0,0 +1,28 @@
+package test20181015.busCard;
+
+public class testBusCard {
+
+	public static void main(String[] args) {
+		//충전소 
+		
+	}
+	
+
+}
+class busCardItem{
+	
+	
+	int id ;
+	//카테고리값 1이면 초등생 - 450원
+	//2면 청소년면 - 720원
+	//3이면 일반 - 900원
+	//4면 우대 - 0원
+	int category;
+	int leftMoney;
+	public busCardItem() {
+		
+		
+	}
+	
+	
+}
