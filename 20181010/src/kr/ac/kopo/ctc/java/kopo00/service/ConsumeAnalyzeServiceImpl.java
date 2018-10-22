f80e33fbbaf972f2b98a67de74fe2950f5ec6ba2
401ST-012 <401ST-012@DESKTOP-N5H3JDG>
2018-10-22 21:03:23 +0900

    1022

--- 20181010/src/kr/ac/kopo/ctc/java/kopo00/service/ConsumeAnalyzeServiceImpl.java
+++ 20181010/src/kr/ac/kopo/ctc/java/kopo00/service/ConsumeAnalyzeServiceImpl.java
@@ -11,13 +11,15 @@ public class ConsumeAnalyzeServiceImpl implements ConsumeAnalyzeService{
 	
 	@Override
 	public List<ConsumingItem> consumingAnalyze(int year, int month) {
+		// 사용자로 부터 입력 받은 년도와 월로 월별 목록가져오기
 		List<AccountItem> items = accountItemService.selectAllByMonth(year, month);
 //		식비, 300000, 0.2 => ConsumingItem
 //		의류, 200000, 0.13 => ConsumingItem
 //		교육, 500000, 0.33 => ConsumingItem
 //		월세, 500000, 0.33 => ConsumingItem
-		//각각 저장할 저장소 준비
+
 		List<ConsumingItem> result = new ArrayList<ConsumingItem>();
+		//각각 타입의 종류별로 저장할 컨슈밍 아이템 저장소 객체 생성
 		ConsumingItem item1 = new ConsumingItem(); // 식비
 		item1.setName("식비");
 		ConsumingItem item2 = new ConsumingItem(); // 의류
@@ -27,6 +29,11 @@ public class ConsumeAnalyzeServiceImpl implements ConsumeAnalyzeService{
 		ConsumingItem item4 = new ConsumingItem(); // 주거
 		item4.setName("주거");
 		
+		// 가져온 월별 목록 중 타입 값에 따라 나누고
+		//컨슈밍 아이템 합계량에 개별 게시물의 카드사용료, 현금 사용를 추출하여 
+		//컨슈밍 아이템 합계 항목에 더한다.
+		//item1.getAmount()를 가져 와서 더하는 이유는 for each 문
+		//반복할때 저장한 값에 다시 더해야 하기 때문
 		for (AccountItem item : items) {
 			if (item.getType().equals("식비")) {
 				item1.setAmount(item1.getAmount() + item.getCard() + item.getCash());
